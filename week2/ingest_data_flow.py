c5953a609caab0d0394df01cb017db60265c61d6
Carter <carter.chene26@gmail.com>
2023-01-29 21:30:20 -0700

    prefect to gcs

--- week2/ingest_data_flow.py
+++ week2/ingest_data_flow.py
@@ -20,28 +20,25 @@ def clean_data(data: pd.DataFrame) -> pd.DataFrame:
     data = data[data['passenger_count'] != 0]
     return data
 
+@task 
+def write_to_local(data: pd.DataFrame, color: str) -> None:
+    """Load the dataframe to local parquet"""
+    data.to_parquet(f"taxi-data/{color}_taxi.parquet")
+
 @task
-def upload_to_gcs(data: pd.DataFrame, port: str, dbname: str) -> None:
+def upload_to_gcs(data: pd.DataFrame, color: str) -> None:
     """Move the cleaned data to google cloud storage bucket."""
     # Create a storage client
-    storage_client = storage.Client()
-    # Get bucket from client
-    bucket = storage_client.bucket("green_taxi_csvs")
-    # Upload
-    blob = bucket.blob("green_taxi")
-    blob.upload_from_file()
-    connection_string = f"postgresql://root:root@localhost:{port}/{dbname}"
-    engine = create_engine(connection_string)
-    data.head(n=0).to_sql(name='green_taxi_data_gh', con=engine, if_exists='replace')
-    data.to_sql(name='green_taxi_data_gh',con=engine,if_exists='append')
-
+    gcp_bucket= GcsBucket.load("gcs-csv-storage")
+    gcp_bucket.upload_from_file(f"week2/taxi-data/{color}_taxi.parquet")
 
 @flow
 def ingest_data_flow():
     print("Starting flow...")
     df = read_data()
     df = clean_data(df)
-    ingest_to_pg(df, '5432', 'ny_taxi')
+    write_to_local(df, 'green')
+    upload_to_gcs(df, 'green')
 
 if __name__ == "__main__":
     ingest_data_flow()
